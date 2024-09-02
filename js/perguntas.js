e0c87edc59061ebb6a247c17db59698c193d8280
gabriell308 <de.souza08@escola.pr.gov.br>
2024-09-02 08:00:55 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,47 @@
+export const perguntas = [
+    {
+        enunciado: "Qual era o foco inicial da Porsche?",
+        alternativas: [
+            {
+                texto: "alternativa a",
+                afirmacao: [
+                    "A Porsche oferecia consultoria e desenvolvimento de projetos automotivos para outros fabricantes",
+                    ""
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "alternativa b",
+                afirmacao: [
+                    " A empresa não fabricava carros próprios até depois da Segunda Guerra Mundial.",
+                    ""
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quem foi o primeiro modelo de carro produzido pela Porsche?",
+        alternativas: [
+            {
+                texto: "alternativa a",
+                afirmacao: [
+                    "O primeiro modelo foi o Porsche 356, lançado em 1948.",
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "alternativa b",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+
+];
